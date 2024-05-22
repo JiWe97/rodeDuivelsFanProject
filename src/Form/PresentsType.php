200832b2e884ba4cec0784282e818b8b72aebeed
Jill Wets <wetsjill@gmail.com>
2024-05-22 09:15:14 +0200

    Small additions in css and attempt to display pictures instead of dropdown to choose present -> not finished

--- src/Form/PresentsType.php
+++ src/Form/PresentsType.php
@@ -13,20 +13,34 @@ class PresentsType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $choices = [
-            'Sjaal 1' => 'option_1',
-            'Sjaal 2' => 'option_2',
-            'Pet' => 'option_3',
-            'Vlag' => 'option_4',
+            'Sjaal 1' => 'sjaal_1',
+            'Sjaal 2' => 'sjaal_2',
+            'Pet' => 'pet',
+            'Vlag' => 'vlag',
         ];
 
-        $builder
+        $builder 
             ->add('choiceField', ChoiceType::class, [
                 'choices' => $choices,
+                'choice_attr' => function($choice, $key, $value) use ($choices) {
+                    // Define image paths for each choice
+                    $imagePaths = [
+                        'sjaal_1' => 'images/sjaal_1.jpg',
+                        'sjaal_2' => 'images/sjaal_2.jpg',
+                        'pet' => 'images/pet.jpg',
+                        'vlag' => 'images/vlag.jpg',
+                    ];
+
+                    // Retrieve image path for the current choice
+                    $imagePath = $imagePaths[$value] ?? '';
+
+                    // Set data-image attribute with the image path
+                    return ['data-image' => $imagePath];
+                },
             ])
             ->add('submit', SubmitType::class)
         ;
     }
-
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
